0269619a640c7b13cf4d20403aac40ffce757e8d
Syed Asil Ali <asilshah17@gmail.com>
2023-02-07 13:24:36 +0500

    Chapter 3 : Functional Components

--- myapp/src/header.js
+++ myapp/src/header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const header = () => {
+const Header = () => {
   return (
     <header>
         <h1>Groceries List</h1>
@@ -8,4 +8,4 @@ const header = () => {
   )
 }
 
-export default header
+export default Header
