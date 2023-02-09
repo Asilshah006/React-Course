c2d53ae914df9bfe6a81882a1c670426c0574457
Syed Asil Ali <asilshah17@gmail.com>
2023-02-09 15:14:02 +0500

    Chapter 5 : UseState / Hooks

--- myapp/src/Content.js
+++ myapp/src/Content.js
@@ -1,31 +1,34 @@
 import React from 'react'
+import { useState } from 'react';
+
 
 const Content = () => {
-    const handleNameChange = () =>{
+  const [name, setName] =useState('Dave');
+  const [count, setCount] = useState(0);
+ 
+  const handleNameChange = () =>{
         const names = ['bob' , 'Kevin' , 'asil'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        setName(names[int]);        
       }
 
       const handleClick =() =>{
-        console.log('You Clicked it');
+        setCount(count + 1);
+        console.log(count);
       }
       const handleClick2 =(name) =>{
-        console.log(`${name} was clicked`);
+       console.log(count) 
       }
-      const handleClick3 =(e) =>{
-        console.log(e.target.innerText);
-      }
-
+    
     return (
    
     <main>
-            <p onDoubleClick={handleClick()}>
-                Hello {handleNameChange()}! 
+            <p onDoubleClick={handleClick}>
+                Hello {name}! 
             </p>
-            <button onClick={handleClick}>Click it</button>
-            <button onClick={() => {handleClick2('Asil')}}>Click it</button>
-            <button onClick={(e) => handleClick3(e)}>Click it</button>
+            <button onClick={handleNameChange}>Change Name</button>
+            <button onClick={handleClick}>{count}</button>
+            <button onClick={handleClick2}>Click it</button>
     </main>
   )
 }
