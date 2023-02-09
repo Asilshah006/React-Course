58e3b97ae82ba45b432992f8d190378782494a0e
Syed Asil Ali <asilshah17@gmail.com>
2023-02-09 10:51:22 +0500

    Chapter 5 : Click Events

--- myapp/src/Content.js
+++ myapp/src/Content.js
@@ -7,12 +7,25 @@ const Content = () => {
         return names[int];
       }
 
+      const handleClick =() =>{
+        console.log('You Clicked it');
+      }
+      const handleClick2 =(name) =>{
+        console.log(`${name} was clicked`);
+      }
+      const handleClick3 =(e) =>{
+        console.log(e.target.innerText);
+      }
+
     return (
    
     <main>
-            <p>
-                Hello {handleNameChange()}
+            <p onDoubleClick={handleClick()}>
+                Hello {handleNameChange()}! 
             </p>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={() => {handleClick2('Asil')}}>Click it</button>
+            <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   )
 }
